4679b62e103ab7650b048ac0967c42cbf46685d5
Tomasz Jakut <vepomoc@gmail.com>
2017-11-12 19:59:06 +0100

    chore(dist): clean after release [ci skip]

--- dist/is-ci.es2015.js
+++
@@ -1,2 +0,0 @@
-/*! @comandeer/is-ci v0.0.0 | (c) 2017 Comandeer | MIT license (see LICENSE) */import isCI from'is-ci';function emitExitCode(a){const b=!0===a?0:1;process.exit(b)}function isCICLI(){emitExitCode(isCI)}export default isCICLI;
-//# sourceMappingURL=is-ci.es2015.js.map
