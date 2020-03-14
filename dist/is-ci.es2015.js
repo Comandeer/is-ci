f0d55a8e6a776652ed7aa807a3be9379a6b994c0
Comandeer <vepomoc@gmail.com>
2020-03-14 18:52:16 +0100

    chore(dist): clean after release [ci skip]

--- dist/is-ci.es2015.js
+++
@@ -1,3 +0,0 @@
-/*! @comandeer/is-ci v2.0.0 | (c) 2020 Comandeer | MIT license (see LICENSE) */
-import isCI from"is-ci";function emitExitCode(a){const b=!0===a?0:1;process.exit(b)}function isCICLI(){emitExitCode(isCI)}export default isCICLI;
-//# sourceMappingURL=is-ci.es2015.js.map
